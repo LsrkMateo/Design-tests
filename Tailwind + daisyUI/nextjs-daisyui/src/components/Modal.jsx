ed50a3441db12549abf27e6a3207a6ef663e12ce
LsrkMateo <121040035+LsrkMateo@users.noreply.github.com>
2023-04-21 22:46:25 -0500

    Corregi el modal :D

--- Tailwind + daisyUI/nextjs-daisyui/src/components/Modal.jsx
+++ Tailwind + daisyUI/nextjs-daisyui/src/components/Modal.jsx
@@ -1,24 +1,28 @@
 export default function Modal() {
   return (
     <>
-      <label htmlFor="my-modal" className="btn">
-        Robux gratis 😎
+      {/* The button to open modal */}
+      <label htmlFor="my-modal-3" className="btn">
+        open modal
       </label>
 
-      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
       <div className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">
-            Felicitaciones!
+        <div className="modal-box relative">
+          <label
+            htmlFor="my-modal-3"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-lg font-bold">
+            Congratulations random Internet user!
           </h3>
           <p className="py-4">
-            Abriste un modal completamente inutil y sin funcion alguna, que tengas un excelente dia!
+            You've been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
           </p>
-          <div className="modal-action">
-            <label htmlFor="my-modal" className="btn">
-              Yay!
-            </label>
-          </div>
         </div>
       </div>
     </>
